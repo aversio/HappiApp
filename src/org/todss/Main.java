a14dda2b70dd9c69a896a0a3f48c7384db092bea
Displee <displee@bitbucket.org>
2017-04-06 11:06:34 +0200

    Add interface.

--- src/org/todss/Main.java
+++ src/org/todss/Main.java
@@ -1,9 +1,18 @@
-package org.todss;
+package org.todss;/**
+ * Created by Displee on 6-4-2017.
+ */
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
 	public static void main(String[] args) {
-		System.out.println("test");
+		launch(args);
 	}
 
+	@Override
+	public void start(Stage primaryStage) {
+
+	}
 }
---
+++ src/org/todss/MainController.java
@@ -0,0 +1,8 @@
+package org.todss;
+
+/**
+ * Created by Displee on 6-4-2017.
+ */
+public class MainController {
+
+}
