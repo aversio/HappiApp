2d56732de331bfbf7328f6d71a010a7dd85b8c39
Displee <displee@bitbucket.org>
2017-04-06 11:09:07 +0200

    Add interface and styling.

--- src/org/todss/Main.java
+++ src/org/todss/Main.java
@@ -1,18 +1,34 @@
-package org.todss;/**
- * Created by Displee on 6-4-2017.
- */
+package org.todss;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.net.URL;
+
 public class Main extends Application {
+
+	/**
+	 * The controller of this application.
+	 */
+	private static MainController controller;
 
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
-	public void start(Stage primaryStage) {
-
+	public void start(Stage stage) throws Exception {
+		final FXMLLoader loader = new FXMLLoader(new URL("file:data/design/main.fxml"));
+		final BorderPane pane = loader.load();
+		controller = loader.getController();
+		controller.setStage(stage);
+		pane.getStylesheets().add("file:data/css/style.css");
+		stage.setScene(new Scene(pane, pane.getPrefWidth(), pane.getPrefHeight(), true));
+		stage.show();
 	}
+
 }
--- src/org/todss/MainController.java
+++ src/org/todss/MainController.java
@@ -1,8 +1,66 @@
 package org.todss;
 
-/**
- * Created by Displee on 6-4-2017.
- */
-public class MainController {
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+	/**
+	 * The stage of this application.
+	 */
+	private Stage stage;
+
+	@FXML
+	private BorderPane mainPane;
+
+	@FXML
+	private DatePicker departurePicker;
+
+	@FXML
+	private DatePicker arrivalPicker;
+
+	@FXML
+	private ListView listView;
+
+	@FXML
+	private GridPane grid;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		FlowPane pane = (FlowPane) grid.getChildren().get(0 * 8 + 2);
+		System.out.println(pane.getStyleClass());
+	}
+
+	public Node getNode(int row, int column) {
+		Node result = null;
+		ObservableList<Node> childrens = grid.getChildren();
+		for (Node node : childrens) {
+			if(grid.getRowIndex(node) == row && grid.getColumnIndex(node) == column) {
+				result = node;
+				break;
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * Set the main stage of this application.
+	 * @param stage The stage to set.
+	 */
+	public void setStage(Stage stage) {
+		this.stage = stage;
+	}
 
 }
